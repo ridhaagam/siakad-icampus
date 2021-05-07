503ff9e573e437e9bb6bea840a32519adbc32f05
ridhaagam <71159151+ridhaagam@users.noreply.github.com>
2021-05-08 06:26:23 +0800

    git push

--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -6,8 +6,10 @@
      </div>
      <!-- /.navbar-collapse -->
      <!-- Navbar Right Menu -->
+
      <div class="navbar-custom-menu">
        <ul class="nav navbar-nav">
+        
          <!-- Messages: style can be found in dropdown.less-->
          <li class="dropdown messages-menu">
 
@@ -19,13 +21,41 @@
          <li class="dropdown user user-menu">
            <!-- Menu Toggle Button -->
            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-             <!-- hidden-xs hides the username on small devices so only the image appears. -->
-             <span class="hidden-xs"><b>Log in</b></span>
+             <?php if(session()->get('username') == "") { ?>
+             <li><a href="<?= base_url('login') ?>"><span class="hidden-xs"><b>Log in</b></span></a></li>
            </a>
-           
+                <!-- LOGGED IN -->
+           <?php } else { ?> 
+             <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs">Alexander Pierce</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Alexander Pierce - Web Developer
+                  <small>Member since Nov. 2012</small>
+                </p>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul> 
          </li>
+         <?php } ?>
        </ul>
      </div>
+     <!-- LOGGED IN -->
      <!-- /.navbar-custom-menu -->
      </div>
      <!-- /.container-fluid -->
