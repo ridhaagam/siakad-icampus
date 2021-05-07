6b99652d97bc81424dbbcc318c4186b26039267e
ridhaagam <71159151+ridhaagam@users.noreply.github.com>
2021-05-08 00:23:42 +0800

    git push

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,2 +1,22 @@
-<div= class="row">
-<img class="img-responsive pad" src="<?= base_url('homepage/umm.png')?>" alt="Photo"></div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="text-center">
+            <h1><b>Selamat Datang</b></h1>
+        </div>
+    </div>
+    <div class="col-sm-12">
+        <div class="text-center">
+            <h1><b>SISTEM INFORMASI AKADEMIK KAMPUS</b></h1>
+            <h2><b>UNIVERSITAS MUHAMMADIYAH MALANG</b></h2>
+            <h3><b>UMM SINCE 1964</b></h3>
+        </div>
+    </div>
+<!-- Background image -->
+<div
+  class="bg-image"
+  style="
+    background-image: url('homepage/umm.png');
+    height: 100vh;
+  "
+></div>
+<!-- Background image -->
